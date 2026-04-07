9b4ce0f83501f269a3c1c0c64efb8d1d5d32aab4
ahmedmobark778 <ahmedmobarek429@gmail.com>
2026-04-08 00:25:03 +0200

    Update bot owner information and name

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'THE ahmed BOT'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['201156440341','201156440341','201156440341']
+global.premium = ['201156440341']
+global.packname = 'The ahmed Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
